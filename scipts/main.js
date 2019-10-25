01ed7295f118de516ec975ff91d5b0a84e335d7b
kevin <chengyu0718@hotmail.com>
2019-10-25 09:59:40 -0700

    made it to first 151 pokemons

--- scipts/main.js
+++ scipts/main.js
@@ -12,10 +12,12 @@ axios
 
 let body = document.getElementById("pokedex");
 function pokeDex(div, data) {
-  for (i = 0; i < 3; i++) {
+  for (i = 0; i < 151; i++) {
     let cardDiv = document.createElement("div");
     cardDiv.classList.add("card-div");
+    cardDiv.id = data[i].name;
     let cardName = document.createElement("h2");
+    cardName.classList.add("poke-name");
     var text = document.createTextNode(data[i].name);
     cardName.appendChild(text);
     let cardImg = document.createElement("img");
@@ -23,6 +25,7 @@ function pokeDex(div, data) {
       cardImg.src = result.data.sprites.front_default;
     });
     let cardType = document.createElement("h3");
+    cardType.classList.add("poke-type");
     // cardType.appendChild(text);
     let pokeType = "";
     axios.get(data[i].url).then(function(result) {
@@ -37,5 +40,27 @@ function pokeDex(div, data) {
     cardDiv.appendChild(cardImg);
     cardDiv.appendChild(cardType);
     div.appendChild(cardDiv);
+  }
+}
+function searchFilter() {
+  var input = document.getElementById("search-box");
+  var filter = input.value.toUpperCase();
+  var div = document.getElementsByClassName("card-div");
+  for (i = 0; i < div.length; i++) {
+    sText = div[i].getElementsByClassName("poke-name")[0];
+    sText2 = div[i].getElementsByClassName("poke-type")[0];
+    // sText3 = div[i].getElementsByClassName("stock-id")[0];
+    var txtValue = sText.textContent || sText.innerText;
+    var txtValue2 = sText2.textContent || sText.innerText;
+    // var txtValue3 = sText3.textContent || sText.innerText;
+    if (
+      txtValue.toUpperCase().indexOf(filter) > -1 ||
+      txtValue2.toUpperCase().indexOf(filter) > -1
+      // txtValue3.toUpperCase().indexOf(filter) > -1
+    ) {
+      div[i].style.display = "";
+    } else {
+      div[i].style.display = "none";
+    }
   }
 }
